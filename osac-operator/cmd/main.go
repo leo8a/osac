0a807167065912c09bc164177c7260b027205217
root <root@macb4835100b12a.infra.se-lab.eng.rdu2.dc.redhat.com>
2026-09-22 15:49:29 +0000

    OSAC-5505: Resolve BMaaS auto-EIP targets from fulfillment API
    Signed-off-by: root <root@macb4835100b12a.infra.se-lab.eng.rdu2.dc.redhat.com>

--- osac-operator/cmd/main.go
+++ osac-operator/cmd/main.go
@@ -1013,11 +1013,15 @@ func setupExternalIPAttachmentControllers(
 	resolver *dispatcher.Resolver, networkClassesClient privatev1.NetworkClassesClient,
 	networkProvisioningEnabled bool, enableBareMetalInstance bool,
 ) error {
+	var bareMetalInstancesClient privatev1.BareMetalInstancesClient
+	if grpcConn != nil {
+		bareMetalInstancesClient = privatev1.NewBareMetalInstancesClient(grpcConn)
+	}
 	reconciler := controller.NewExternalIPAttachmentReconciler(
 		mgr, networkingNamespace, computeInstanceNamespace,
 		clusterOrderNamespace, baremetalInstanceNamespace,
 		provider, statusPollInterval, maxJobHistory, targetCluster,
-		resolver, networkClassesClient,
+		resolver, networkClassesClient, bareMetalInstancesClient,
 	)
 	reconciler.NetworkProvisioningEnabled = networkProvisioningEnabled
 	reconciler.BareMetalInstanceEnabled = enableBareMetalInstance
